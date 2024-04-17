82a6e449bf3aacfc2531bede5b35a1ea03047ec8
rafael-mingossi <rafaelmingossi@gmail.com>
2024-04-17 14:21:21 +1000

    basic structure and packages

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        safe: false,
+        allowUndefined: true,
+        verbose: false,
+      },
+    ],
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@screens': './src/screens',
+          '@env': ['node_modules/react-native-dotenv'],
+        },
+      },
+    ],
+  ],
 };
---
+++ src/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {Dashboard} from '@screens';
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <Text style={styles.sectionContainer}>Hooooome</Text>
+      <Dashboard />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    color: 'red',
+  },
+});
+
+export default App;
